35e19be6bb37c129cf4851c6f16d3bec2aae2d7a
emadPoursina <emad.puursina@gmail.com>
2021-03-06 13:16:16 +0330

    get request token

--- src/pocket.js
+++ src/pocket.js
@@ -1,46 +1,48 @@
 const axios = require('axios').default;
-const { pocketInfo } = require('../.config');
+const open = require('open');
 
-const url = 'https://getpocket.com/v3/oauth/request';
-const body = {
-  consumer_key: pocketInfo.consumer_key,
-  redirect_uri: "pocketapp1234:authorizationFinished"
-};
+const consumerKey = "96126-d71e8b7e2255a5075eb0c83c";
+const redirectUri = "https://www.google.com";
 
-const option = {
-  headers: {
-    'Content-Type': 'application/json; charset=UTF-8',
-    'X-Accept': 'application/json'
+/**
+ * Return an array of pocket account articles
+ */
+async function getArticle() {
+  try {
+    const requestToken = await obtainRequestToken(consumerKey, redirectUri);
+    console.log(requestToken);
+  } catch (error) {
+    console.log(new Error(error));
   }
-};
-
-function getArticle() {
-  axios.post(url, body, option)
-  .then(res => {
-    if(res.status === 200) {
-      const requestToken = res.data.code;
-    }
-  })
-  .catch(function (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-  });
 }
 
-getArticle();
+/**
+ * 
+ * @param {String} consumerKey 
+ * @param {String} redirectUri 
+ * @returns String - Request Token
+ */
+async function obtainRequestToken(consumerKey, redirectUri) {
+  const requestUrl = 'https://getpocket.com/v3/oauth/request';
+  const requestBody = {
+    consumer_key: consumerKey,
+    redirect_uri: redirectUri,
+  }
+
+  const option = {
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'X-Accept': 'application/json'
+    }
+  };
+  
+  const response = await axios.post(requestUrl, requestBody, option)
+
+  if(response.status === 200) {
+    return response.data.code;
+  }else {
+    throw new Error(response.status.toString());
+  }
+}
 
 module.exports = getArticle;
