309ec6c568f01ba35883631fe152852761d1729e
emadPoursina <emad.puursina@gmail.com>
2021-03-07 20:09:33 +0330

    add getWebPage function

--- src/converter.js
+++ src/converter.js
@@ -1,3 +1,5 @@
+const axios = require('axios').default;
+
 /**
  * 
  * @param {String} url 
@@ -7,5 +9,37 @@
 function urlToEpub(url, article, outputDir = __dirname + '/../article/') {
 }
 
+/**
+ * 
+ * @param {String} url 
+ * @returns Webpage content
+ */
+function getWebPage(url) {
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(error => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.headers['x-error-code'], error.response.headers['x-error']);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config.url, error.config.method, error.config.headers, error.config.data);
+
+        reject('Request Faild');
+      });
+  });
+}
 
 module.exports = urlToEpub;
