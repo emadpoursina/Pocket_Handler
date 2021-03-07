b216c7d913a031f64546c3b124f197874d1d006c
emadPoursina <emad.puursina@gmail.com>
2021-03-07 11:26:13 +0330

    refactor and get Articles

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const getArticle = require('./src/pocket');
+const Pocket = require('./src/pocket');
 
 async function main() {
-  // Get articles from pocket server
-  const articles = await getArticle();
+  const pocket = await Pocket.build('	96126-d71e8b7e2255a5075eb0c83c', 'https://www.google.com');
+
+  // Get articles from pocket server with custome filter
+  const articles = await pocket.getArticle({
+    count: 1,
+    detailType: 'simple',
+  });
 }
 
 main();
