8a53a62626c2d1560306a40d0e58da4de40263c0
emadPoursina <emad.puursina@gmail.com>
2021-03-07 20:07:28 +0330

    add urlToEpub and export it

---
+++ src/converter.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {String} url 
+ * @param {Object} article 
+ * @param {String} outputPath 
+ */
+function urlToEpub(url, article, outputDir = __dirname + '/../article/') {
+}
+
+
+module.exports = urlToEpub;
