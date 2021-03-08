d31708372c9d3ab84ea68a11e29bb3bb9ec13b03
emadPoursina <emad.puursina@gmail.com>
2021-03-08 15:21:55 +0330

    change send request method and refactor: change main function parameters change getWebPage main commenting

--- src/converter.js
+++ src/converter.js
@@ -15,7 +15,7 @@ const tmpDir = '/tmp/ebookConverter/';
  * @param {Object} article 
  * @param {String} outputPath 
  */
-function urlToMobi(url, article, outputDir = __dirname + '/../article/') {
+function urlToMobi(url, outputDir = __dirname + '/../article/') {
   return new Promise((resolve, reject) => {
     getWebPage(url)
       .then(data => {
@@ -34,33 +34,44 @@ function urlToMobi(url, article, outputDir = __dirname + '/../article/') {
 /**
  * 
  * @param {String} url 
- * @returns Webpage content
+ * @returns A readable stream of webpage content
  */
 function getWebPage(url) {
   return new Promise((resolve, reject) => {
-    axios.get(url)
-      .then(response => {
-        resolve(response.data);
-      })
-      .catch(error => {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.headers['x-error-code'], error.response.headers['x-error']);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
-        }
-        console.log(error.config.url, error.config.method, error.config.headers, error.config.data);
+    axios({
+      method: 'get',
+      url,
+      responseType: 'stream',
+    })
+    .then(response => {
+      const dataStream = response.data;
 
-        reject('Request Faild');
+      dataStream.on('error', (err) => {
+        console.log('Download faild: ' + err);
+        throw new Error('Download faild');
       });
+
+      resolve(dataStream);
+    })
+    .catch(error => {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.headers['x-error-code'], error.response.headers['x-error']);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config.url, error.config.method, error.config.headers, error.config.data);
+
+      reject('Request Faild');
+    });
   });
 }
 
