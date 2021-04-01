ef0b6e1b48c2d5e4d7b3603e94320bb61f33c237
emadPoursina <emad.puursina@gmail.com>
2021-04-01 12:14:47 +0430

    add some commenting and fix some mistakes

--- app.js
+++ app.js
@@ -1,47 +1,59 @@
 const Pocket = require('./src/pocket');
 const inquirer = require('inquirer');
 const urlToMobi = require('./src/converter');
+const Helper = require('./src/Helper');
 
+// Main menue question
 const questions = [
   {
     type: 'rawlist',
     name: 'job',
     message: 'What should I do??',
-    choices: ['1)Get articles', '2)Send to kindle (WireFull)', '3)Get and send article to kindl'],
+    choices: [
+      '1)Get articles',
+      '2)Html to Mobi',
+      '3)Send to kindle (wirefull)',
+      '4)Send to kindle (wireless)'],
   },
 ];
+// Where docs will be saved
 const outputFolder = __dirname + '/article/';
 
 async function main() {
   try {
-    const pocket = await Pocket.build('96126-d71e8b7e2255a5075eb0c83c', 'https://www.google.com');
+    //Autherize pocket
+    //const pocket = await Pocket.build('96126-d71e8b7e2255a5075eb0c83c', 'https://www.google.com');
 
     console.log('Hi, welcome to Node Pocket app');
 
     while(true) {
       const choice = (await inquirer.prompt(questions)).job[0];
       
-      if(choice === '1') {
-        let articles = await pocket.getArticle({
-          count: 1,
-          detailType: 'simple',
-        });
+      switch (choice) {
+        case '1':
+          // Get articles info from pocket site
+          let articles = await pocket.getArticle({
+            count: 1,
+            detailType: 'simple',
+          });
+          console.log('Articles received!');
 
-        console.log('Articles received!');
+          // Converted articles
+          const converters = []; 
 
-        const converters = []; 
+          // Convert articles
+          articles = Object.values(articles);
+          articles.forEach(article => {
+            article.path = outputFolder + article.resolved_title + '.mobi';
+            converters.push(urlToMobi(article.resolved_url, article.path));
+          });
+          await Promise.all(converters);
 
-        articles = Object.values(articles);
-        articles.forEach(article => {
-          article.path = outputFolder + article.resolved_title + '.mobi';
-          converters.push(urlToMobi(article.resolved_url, article.path));
-        });
-
-        await Promise.all(converters);
-
-        console.log('Finished');
-      }else if(choice === '2') {
-
+          console.log('Finished');
+          break;
+        default:
+          console.log('Invalid choice!');
+          break;
       }
     }
   } catch (error) {
